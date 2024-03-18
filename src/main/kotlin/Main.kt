1211e15fade9f536d5132bcffb09348b0391b9b1
elkin-pinto <elkin.pinto.7e7@itb.cat>
2024-03-18 13:54:20 +0100

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
 
+import org.postgresql.util.PSQLException
 import java.sql.Connection
 import java.sql.DriverManager
+
+
+data class Usuario(val dni:String,val apenom:String, val direc:String,val pobla:String, val telef:String)
+
+val usuariosNuevos = listOf(
+    Usuario("98765432", "Gómez Pérez, Juan", "C/Gran Vía, 10", "Barcelona", "932345678"),
+    Usuario("34567891", "Martínez López, Ana", "Avda. Libertad, 15", "Valencia", "963456789"),
+    Usuario("45678901", "Sánchez Rodríguez, Pablo", "C/Alcalá, 45", "Madrid", "910111213")
+)
 
 fun main() {
     val jdbcUrl = "jdbc:postgresql://localhost:5432/school"
     val connection = DriverManager.getConnection(jdbcUrl, "sjo", "")
-
+    Exercici5(connection)
     connection.close()
 }
 
@@ -15,9 +25,6 @@ fun Exercici1(connection: Connection) {
     val query = connection.createStatement()
     val sql = "SELECT * FROM alumnos"
     val result = query.executeQuery(sql)
-
-
-
 
     while (result.next()) {
         val id = result.getString("dni")
@@ -53,8 +60,7 @@ fun Exercici3(connection:Connection) {
     val dni = "4448242"
 
 
-    val query = connection.prepareStatement("SELECT * FROM notas " +
-            "                                   WHERE dni = ?")
+    val query = connection.prepareStatement("SELECT * FROM notas WHERE dni = ?")
     query.setString(1, dni) // dni es la variable 4448242
 
 
@@ -73,4 +79,70 @@ fun Exercici3(connection:Connection) {
 
     result.close()
     query.close()
+}
+
+fun Exercici4(connection:Connection) {
+    val query = connection.prepareStatement("INSERT INTO alumnos VALUES (?,?,?,?,?)")
+    var quantitatInserts:Int = 0
+
+
+
+    try {
+        for (usuario in usuariosNuevos) {
+            query.setString(1,usuario.dni)
+            query.setString(2,usuario.apenom)
+            query.setString(3,usuario.direc)
+            query.setString(4,usuario.pobla)
+            query.setString(5,usuario.telef)
+            quantitatInserts += query.executeUpdate()
+        }
+        println("S'han fet una quantitat de $quantitatInserts")
+    } catch (e:PSQLException){
+        println("Probablement has intentat inserir un DNI ja existent")
+    } finally {
+        query.close()
+    }
+}
+
+fun Exercici5(connection:Connection) {
+    val asignaturas:MutableList<Int> = mutableListOf()
+
+    var query = connection.createStatement()
+
+
+    val asignaturasABuscar = listOf("FOL","RET")
+    val result = query.executeQuery("SELECT * FROM asignaturas WHERE nombre = '${asignaturasABuscar[0]}' OR nombre = '${asignaturasABuscar[1]}'")
+
+
+    while (result.next()) {
+        val idAssignatura = result.getInt("cod")
+        asignaturas.add(idAssignatura)
+    }
+
+    query = connection.prepareStatement("INSERT INTO notas VALUES (?,?,?)")
+    var quantitatInserts:Int = 0
+
+
+    try {
+
+        for (usuario in usuariosNuevos) {
+            query.setString(1,usuario.dni)
+            query.setInt(2, asignaturas[0])
+            query.setInt(3,8)
+            quantitatInserts += query.executeUpdate()
+
+            query.setString(1,usuario.dni)
+            query.setInt(2, asignaturas[1])
+            query.setInt(3,8)
+            quantitatInserts += query.executeUpdate()
+        }
+
+
+        println("S'han fet una quantitat de $quantitatInserts")
+    } catch (e:PSQLException){
+        println("Probablement el usuari no existeix o has duplicat el dni")
+        println(e.message)
+    } finally {
+        query.close()
+    }
 }
