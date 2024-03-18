faf877b1c06d2a68d499fd561ab83fbd69729476
elkin-pinto <elkin.pinto.7e7@itb.cat>
2024-03-18 13:09:26 +0100

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,42 @@
 
+import java.sql.Connection
 import java.sql.DriverManager
 
 fun main() {
     val jdbcUrl = "jdbc:postgresql://localhost:5432/school"
     val connection = DriverManager.getConnection(jdbcUrl, "sjo", "")
 
-    val query = connection.prepareStatement("INSERT INTO alumnos VALUES(?,?,?,?,?)")
-    val result = query.executeQuery()
+    connection.close()
+}
+
+
+fun Exercici1(connection: Connection) {
+
+    val query = connection.createStatement()
+    val sql = "SELECT * FROM alumnos"
+    val result = query.executeQuery(sql)
+
+
+
+
+    while (result.next()) {
+        val id = result.getString("dni")
+        val apenom = result.getString("apenom")
+        val direc = result.getString("direc")
+        val pobla = result.getString("pobla")
+        val telef = result.getString("telef")
+        println("ID :: $id Cognom i Nom :: $apenom Direcció :: " +
+                "$direc Població :: $pobla Telèfon :: $telef")
+    }
+
+    result.close()
+    query.close()
+}
+
+fun Exercici2(connection: Connection) {
+    val query = connection.createStatement()
+    val sql = "SELECT * FROM notas"
+    val result = query.executeQuery(sql)
 
     while (result.next()) {
         val dni = result.getString("dni")
@@ -17,5 +47,30 @@ fun main() {
 
     result.close()
     query.close()
-    connection.close()
+}
+
+fun Exercici3(connection:Connection) {
+    val dni = "4448242"
+
+
+    val query = connection.prepareStatement("SELECT * FROM notas " +
+            "                                   WHERE dni = ?")
+    query.setString(1, dni) // dni es la variable 4448242
+
+
+    val result = query.executeQuery() // executem la query amb el
+
+
+    // mostrem dades amb una iteració
+    println("Notes de l'alumne amb DNI: $dni")
+
+    while (result.next()) {
+        val cod = result.getInt("cod")
+        val nota = result.getInt("nota")
+        println("Codi: $cod Nota: $nota")
+    }
+
+
+    result.close()
+    query.close()
 }
